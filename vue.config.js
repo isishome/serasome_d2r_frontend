0cca7c95f6d69bfde1a8a755896b5b84853c6710
isishome <isishome@hanmail.net>
2022-01-24 13:03:04 +0900

    remove node.js server

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,33 @@
+//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
-  publicPath: '',
+  pages: {
+    index: {
+      entry: 'src/main/d2r.js',
+      template: 'public/d2r.html',
+      filename: 'index.html'
+    }
+  },
+  // configureWebpack: {
+  //   plugins: [new BundleAnalyzerPlugin()]
+  // },
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
+    config.plugins.delete('prefetch-index')
+  },
   pluginOptions: {
+    quasar: {
+      importStrategy: 'kebab',
+      rtlSupport: false,
+      supportIE: true
+    },
     cordovaPath: 'src-cordova'
-  }
+  },
+  transpileDependencies: [
+    'quasar',
+    'highlight.js'
+  ],
+  productionSourceMap: false,
+  outputDir: '../../../publish/serasome_d2r_frontend',
+  assetsDir: 'static'
 }
